f5108fcbf03830424bc343adeabc0055bd94c21e
Gabriel <Gabrielpr93@hotmail.com>
2022-02-02 16:24:11 +0100

    Finalizar login y implementar permisos

--- app/src/main/java/com/example/firebaseapplication/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseapplication/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.firebaseapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -25,14 +27,28 @@ class LoginActivity : AppCompatActivity() {
             val password=loginPassword.text.toString()
 
             if(checkEmpty(email,password)){
+                login(email,password)
 
             }
         }
 
         idButtonRegister.setOnClickListener {
-
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish()
         }
 
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful){
+                    startActivity(Intent(this,MainActivity::class.java))
+                    finish()
+                }else{
+                    Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     private fun checkEmpty(email: String, password: String): Boolean {
